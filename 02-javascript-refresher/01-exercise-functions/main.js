7f4914925451b5a82582a7263c0fadaa65b7bcdb
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2024-11-08 16:06:55 +0100

    ex: add ex description

--- 02-javascript-refresher/01-exercise-functions/main.js
+++ 02-javascript-refresher/01-exercise-functions/main.js
@@ -1,2 +1,12 @@
-import './style.css'
+/*
+Your task is to write a new function that should be named combine and have the following characteristics:
+Accept three input values
+Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
+Return the calculated result
+ */
 
+function combine(a, b, c) {
+    return a * b / c;
+}
+
+console.log(combine(2,2,2))
