d2706f87d48707289cff536657b8a25cf69a7d5a
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-01-22 16:20:43 +0100

    lec 75: editing mode

--- 04-react-essentials-deep-dive/01-lectures/src/components/Player.jsx
+++ 04-react-essentials-deep-dive/01-lectures/src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function clickEdit() {
+        setIsEditing(!isEditing);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    let buttonName = "Edit";
+    if (isEditing) {
+        playerName = <input type="text" required></input>;
+        buttonName = "Save";
+    }
 
     return(
         <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+            {playerName}
           <span className="player-symbol">{symbol}</span>
-          <button>Edit</button>
+          <button onClick={clickEdit}>{buttonName}</button>
         </span>
       </li>
     )
