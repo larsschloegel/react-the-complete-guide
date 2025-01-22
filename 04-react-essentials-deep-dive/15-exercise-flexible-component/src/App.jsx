803db7541ef666a27f97b8c4bfa1dba365a322cf
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-01-22 11:41:56 +0100

    ex: 15

---
+++ 04-react-essentials-deep-dive/15-exercise-flexible-component/src/App.jsx
@@ -0,0 +1,52 @@
+import Button from './Button';
+import HomeIcon from './HomeIcon';
+
+function App() {
+  return (
+     <div id="app">
+      <section>
+        <h2>Filled Button (Default)</h2>
+        <p>
+          <Button>Default</Button>
+        </p>
+        <p>
+          <Button mode="filled">Filled (Default)</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Outline</h2>
+        <p>
+          <Button mode="outline">Outline</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Text-only Button</h2>
+        <p>
+          <Button mode="text">Text</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Icon</h2>
+        <p>
+        <Button Icon={HomeIcon}>Home</Button>
+        </p>
+        <p>
+          
+        </p>
+      </section>
+      <section>
+        <h2>Buttons Should Support Any Props</h2>
+        <p>
+          <Button mode="filled" disabled>
+            Disabled
+          </Button>
+        </p>
+        <p>
+          <Button onClick={() => console.log('Clicked!')}>Click me</Button>
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
