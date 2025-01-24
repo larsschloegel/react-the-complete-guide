d39611e6f09e9e3f8292713ae3438b43df3fd488
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-01-24 15:23:50 +0100

    lec 92: implement draw and game over screen

--- 04-react-essentials-deep-dive/01-lectures/src/App.jsx
+++ 04-react-essentials-deep-dive/01-lectures/src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./components/Gameboard"
 import Player from "./components/Player"
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 const initialGameBoard = [
   [null, null, null],
@@ -33,14 +34,16 @@ function App() {
   }
 
   for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row] [combination[0].column];
-    const secondSquareSymbol = gameBoard[combination[1].row] [combination[1].column];
-    const thirdSquareSymbol = gameBoard[combination[2].row] [combination[2].column];
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
     if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
       winner = firstSquareSymbol;
     }
   }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -58,7 +61,7 @@ function App() {
           <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'} />
           <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
