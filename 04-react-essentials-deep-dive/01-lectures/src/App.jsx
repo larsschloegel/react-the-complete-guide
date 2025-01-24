90d6686c96951e1e62a37854674149bed23b4156
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-01-24 14:30:58 +0100

    lec 87: identifying unessary state

--- 04-react-essentials-deep-dive/01-lectures/src/App.jsx
+++ 04-react-essentials-deep-dive/01-lectures/src/App.jsx
@@ -3,33 +3,37 @@ import GameBoard from "./components/Gameboard"
 import Player from "./components/Player"
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTruns = [
-        {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns];
-       return updatedTruns;
+      return updatedTruns;
     });
   }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'}/>
-          <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'}/>
+          <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'} />
+          <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   )
 }
