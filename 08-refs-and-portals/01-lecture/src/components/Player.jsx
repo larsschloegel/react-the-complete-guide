b6114a3618a0504c613fa4da2ccace1b5c7cfdb0
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-05 15:37:19 +0100

    lec 134: intro refs

--- 08-refs-and-portals/01-lecture/src/components/Player.jsx
+++ 08-refs-and-portals/01-lecture/src/components/Player.jsx
@@ -1,22 +1,18 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 export default function Player() {
+  const playerNameRef = useRef();
   const [playerName, setPlayerName] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(false);
-    setPlayerName(event.target.value);
-  }
-
+  
   function handleClick() {
-    setSubmitted(true);
+    setPlayerName(playerNameRef.current.value);
   }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName : ' unknown entity'}</h2>
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={playerName} />
+        <input type="text" ref={playerNameRef}/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
