74f79defa2d74019059a8f105ab5e6c97f61e441
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-07 15:00:48 +0100

    lec 152: styling new Project and Inputs

--- 09-project-management-app/project-management-app/src/components/NewProject.jsx
+++ 09-project-management-app/project-management-app/src/components/NewProject.jsx
@@ -1,9 +1,9 @@
 import Input from "./Input"
 export default function NewProject() {
-    return <div>
-        <menu>
-            <li><button>Cancel</button></li>
-            <li><button>Save</button></li>
+    return <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+            <li><button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md">Save</button></li>
         </menu>
         <div>
             <Input label="Title"/>
