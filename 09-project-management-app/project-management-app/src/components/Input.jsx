a61f2b1a17ecd7914e1dd928636f97dff2d1ef86
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-07 14:36:41 +0100

    lec 151: add new project and inputs

---
+++ 09-project-management-app/project-management-app/src/components/Input.jsx
@@ -0,0 +1,6 @@
+export default function Input({ label, isTextarea, ...props }) {
+    return <p>
+        <label>{label}</label>
+        {isTextarea ? <textarea {...props} /> : <input  {...props} />}
+    </p>
+}
