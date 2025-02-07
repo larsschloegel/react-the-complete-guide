748b059397f7ff99c371e9ec8ed3401763c59e5e
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-07 12:11:17 +0100

    lec 147: portal

--- 08-refs-and-portals/01-lecture/src/components/ResultModal.jsx
+++ 08-refs-and-portals/01-lecture/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime, remainingTime, onReset }, ref) {
 
@@ -16,16 +17,18 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime, remain
         };
     });
 
-
-    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
-        {userLost && <h2>You lost</h2>}
-        {!userLost && <h2>Your score: {score}</h2>}
-        <p>The target time was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
-        <form method="dialog" onSubmit={onReset}>
-            <button>Close</button>
-        </form>
-    </dialog>;
+    return createPortal(
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    );
 });
 
 export default ResultModal;
