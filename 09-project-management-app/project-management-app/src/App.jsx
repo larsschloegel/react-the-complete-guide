9fe4be330c18cc24fb340f5848e31a59b4374814
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-10 16:56:44 +0100

    lec 154: managing state for create project

--- 09-project-management-app/project-management-app/src/App.jsx
+++ 09-project-management-app/project-management-app/src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projects, setProjects] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjects(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
+
+  let content;
+
+  if (projects.selectedProjectId === null) {
+    content = <NewProject/>
+  } else if (projects.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar/>
-      <NoProjectSelected/>
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
