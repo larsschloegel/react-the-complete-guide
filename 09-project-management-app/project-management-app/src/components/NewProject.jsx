4def6996a8b0b3cab34b85bd8cd11a3eb305a544
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-11 15:17:12 +0100

    lec 157: validation input and use error modal

--- 09-project-management-app/project-management-app/src/components/NewProject.jsx
+++ 09-project-management-app/project-management-app/src/components/NewProject.jsx
@@ -1,6 +1,9 @@
 import { useRef } from "react";
 import Input from "./Input"
-export default function NewProject({onAdd}) {
+import Modal from "./Modal";
+export default function NewProject({ onAdd }) {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -10,6 +13,11 @@ export default function NewProject({onAdd}) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -17,15 +25,25 @@ export default function NewProject({onAdd}) {
         });
     }
 
-    return <div className="w-[35rem] mt-16">
-        <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md" onClick={handleSave}>Save</button></li>
-        </menu>
-        <div>
-            <Input type="text" ref={title} label="Title" />
-            <Input ref={description} label="Description" isTextarea />
-            <Input type="date" ref={dueDate} label="Due Date" />
-        </div>
-    </div>
+    return (
+        <>
+            <Modal ref={modal} buttonLabel="Okay">
+                <h2>Invalid Input</h2>
+                <p>Oops ... looks like you forgot to enter a value.</p>
+                <p>Please make sure you provide a valid value for every input field.</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                    <li><button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md" onClick={handleSave}>Save</button></li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="Title" />
+                    <Input ref={description} label="Description" isTextarea />
+                    <Input type="date" ref={dueDate} label="Due Date" />
+                </div>
+            </div>
+        </>
+    )
+
 }
