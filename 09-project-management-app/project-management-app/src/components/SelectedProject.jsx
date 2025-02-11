6779253899b67a750411434f93a30a2f2d63cedb
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-11 16:44:35 +0100

    lec 161: create component for adding tasks

--- 09-project-management-app/project-management-app/src/components/SelectedProject.jsx
+++ 09-project-management-app/project-management-app/src/components/SelectedProject.jsx
@@ -1,3 +1,4 @@
+import Tasks from "./Tasks";
 export default function SelectedProject({project, onDelete}) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
@@ -15,7 +16,7 @@ export default function SelectedProject({project, onDelete}) {
             <p className="mb-4 text-stone-400">{formattedDate}</p>
             <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
         </header>
-        TASKS
+       <Tasks/>
     </div>
     );
 }
