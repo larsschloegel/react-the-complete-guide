e6f5a6d1784d327ea78cea272a0881a987aa17a3
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-12 17:09:55 +0100

    lec 168: ceating context

--- 10-react-context-use-reducer/01-lecture/src/App.jsx
+++ 10-react-context-use-reducer/01-lecture/src/App.jsx
@@ -4,6 +4,8 @@ import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 import Product from './components/Product.jsx';
+import { CartContext } from './store/shopping-cart-context.jsx';
+
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -68,17 +70,19 @@ function App() {
 
   return (
     <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart}>
-      {DUMMY_PRODUCTS.map((product) => (
-          <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
-          </li>
-        ))}
-      </Shop>
+      <CartContext>
+        <Header
+          cart={shoppingCart}
+          onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+        />
+        <Shop onAddItemToCart={handleAddItemToCart}>
+          {DUMMY_PRODUCTS.map((product) => (
+            <li key={product.id}>
+              <Product {...product} onAddToCart={handleAddItemToCart} />
+            </li>
+          ))}
+        </Shop>
+      </CartContext>
     </>
   );
 }
