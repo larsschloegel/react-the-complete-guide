72fa6bdc944758723134ed64f9a403cf8fb9107d
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-12 17:24:28 +0100

    lec 169: consuming the context

--- 10-react-context-use-reducer/01-lecture/src/App.jsx
+++ 10-react-context-use-reducer/01-lecture/src/App.jsx
@@ -70,7 +70,7 @@ function App() {
 
   return (
     <>
-      <CartContext>
+      <CartContext value={{items: []}}>
         <Header
           cart={shoppingCart}
           onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
