00a62166015cfc8b36efd56ed00917a5fbbc0108
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-18 13:53:28 +0100

    lec 196: shuffling answers

--- 12-project-quiz-app/quiz-app/src/components/Quiz.jsx
+++ 12-project-quiz-app/quiz-app/src/components/Quiz.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
@@ -11,12 +13,22 @@ export default function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={quizCompleteImg} alt="Trophy icon" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+
+    const shuffeldAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffeldAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => <li key={answer} className="answer">
+                    {shuffeldAnswers.map(answer => <li key={answer} className="answer">
                         <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                     </li>)}
                 </ul>
