b7c1bdb87aa77ecd413c5a223154ea64991cb457
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-18 15:20:39 +0100

    lec 200: highlighting selected answers

--- 12-project-quiz-app/quiz-app/src/components/Quiz.jsx
+++ 12-project-quiz-app/quiz-app/src/components/Quiz.jsx
@@ -4,16 +4,29 @@ import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from './QuestionTimer.jsx'
 
 export default function Quiz() {
+    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setAnswerState('answered');
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
-    }, []);
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct');
+            } else {
+                setUserAnswers('wrong');
+            }
+
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000);
+        }, 1000);
+    }, [activeQuestionIndex]);
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -30,12 +43,24 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
-                <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}/>
+                <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffeldAnswers.map(answer => <li key={answer} className="answer">
-                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                    </li>)}
+                    {shuffeldAnswers.map(answer => {
+                        const isSelected = userAnswers[userAnswers.length - 1] === answer;
+                        let cssClass = '';
+                        if(answerState === 'answered' && isSelected) {
+                            cssClass = 'selected';
+                        }
+
+                        if((answerState === 'correct' || answer === 'wrong') && isSelected) {
+                            cssClass = answerState;
+                        }
+
+                        return <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>{answer}</button></li>;
+                    }
+                    )}
                 </ul>
             </div>
         </div>
