9ecbc8db486876a965b44d35f364e8aada3e5367
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-19 14:00:22 +0100

    lec 215: keys for resetting components

--- 13-behind-react-optimization-techniques/01-lecture/src/components/Counter/Counter.jsx
+++ 13-behind-react-optimization-techniques/01-lecture/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback, useMemo } from 'react';
+import { useState, memo, useCallback, useMemo, useEffect } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -33,6 +33,10 @@ const Counter = memo(function Counter({ initialCount }) {
     [initialCount]
   );
 
+  //useEffect(() => {
+  //  setCounterChanges([{ value: initialCount, id: Math.random() * 1000 }]);
+  //}, [initialCount])
+
   // const [counter, setCounter] = useState(initialCount);
   const [counterChanges, setCounterChanges] = useState([
     { value: initialCount, id: Math.random() * 1000 },
@@ -46,7 +50,7 @@ const Counter = memo(function Counter({ initialCount }) {
   const handleDecrement = useCallback(function handleDecrement() {
     // setCounter((prevCounter) => prevCounter - 1);
     setCounterChanges((prevCounterChanges) => [
-      { value: -1, id: Math.random() * 1000 }, 
+      { value: -1, id: Math.random() * 1000 },
       ...prevCounterChanges,
     ]);
   }, []);
