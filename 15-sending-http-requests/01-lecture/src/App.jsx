0c1bbce820d077218048663e1e3b66911cc721ba
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-20 19:21:12 +0100

    lec 241: handle error when update places

--- 15-sending-http-requests/01-lecture/src/App.jsx
+++ 15-sending-http-requests/01-lecture/src/App.jsx
@@ -6,11 +6,13 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { updateUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -37,48 +39,58 @@ function App() {
     try {
       await updateUserPlaces([selectedPlace, ...userPlaces]);
     } catch (error) {
-
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({ message: error.message || 'Failed to update places.' })
     }
   }
 
 
-    const handleRemovePlace = useCallback(async function handleRemovePlace() {
-      setUserPlaces((prevPickedPlaces) =>
-        prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-      );
-
-      setModalIsOpen(false);
-    }, []);
-
-    return (
-      <>
-        <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-          <DeleteConfirmation
-            onCancel={handleStopRemovePlace}
-            onConfirm={handleRemovePlace}
-          />
-        </Modal>
-
-        <header>
-          <img src={logoImg} alt="Stylized globe" />
-          <h1>PlacePicker</h1>
-          <p>
-            Create your personal collection of places you would like to visit or
-            you have visited.
-          </p>
-        </header>
-        <main>
-          <Places
-            title="I'd like to visit ..."
-            fallbackText="Select the places you would like to visit below."
-            places={userPlaces}
-            onSelectPlace={handleStartRemovePlace}
-          />
-
-          <AvailablePlaces onSelectPlace={handleSelectPlace} />
-        </main>
-      </>
+  const handleRemovePlace = useCallback(async function handleRemovePlace() {
+    setUserPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
+
+    setModalIsOpen(false);
+  }, []);
+
+  function handleError() {
+    setErrorUpdatingPlaces(null);
   }
 
-  export default App;
+  return (
+    <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        {errorUpdatingPlaces && (
+          <Error title="An error occured!" message={errorUpdatingPlaces.message} onConfirm={handleError} />
+        )}
+      </Modal>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText="Select the places you would like to visit below."
+          places={userPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+
+        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+      </main>
+    </>
+  );
+}
+
+export default App;
