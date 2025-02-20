6e6eef6c7b9ce43a0d0aba508daee9cf7c9eb9fc
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-20 19:33:28 +0100

    lec 243: fetching selected places

--- 15-sending-http-requests/01-lecture/src/http.js
+++ 15-sending-http-requests/01-lecture/src/http.js
@@ -27,3 +27,15 @@ export async function updateUserPlaces(places) {
 
     return resData.message;
 }
+
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const resData = await response.json();
+    // 200, 300 => ok
+    // 400, 500 => not ok
+    if (!response.ok) {
+        throw new Error('Failed to fetch user places');
+    }
+
+    return resData.places;
+}
