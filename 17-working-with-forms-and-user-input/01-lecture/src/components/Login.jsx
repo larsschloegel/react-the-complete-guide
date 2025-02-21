860df9d7de7bf81eeb92df9248f7bb27fe0c6b9c
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-21 16:14:38 +0100

    lec 254: handle multiple user inputs with generic handler

--- 17-working-with-forms-and-user-input/01-lecture/src/components/Login.jsx
+++ 17-working-with-forms-and-user-input/01-lecture/src/components/Login.jsx
@@ -1,23 +1,44 @@
+import { use } from "react";
+import { useState } from "react";
+
 export default function Login() {
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleSubmit() {
+    event.preventDefault();
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(event, identifier) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: event.target.value
+    }))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(event) => handleInputChange(event,'email')} value={enteredValues.email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event) => handleInputChange(event,'password')} value={enteredValues.password}/>
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" >Login</button>
       </p>
     </form>
   );
