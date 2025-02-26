d8d7fd8a0a5563034c68c4b65a34568b3411be80
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-26 18:04:15 +0100

    lec 258: validation on every keystroke

---
+++ 17-working-with-forms-and-user-input/01-lecture/src/components/StateLogin.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function Login() {
+
+    const [enteredValues, setEnteredValues] = useState({
+        email: '',
+        password: ''
+    });
+
+    const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
+    function handleSubmit() {
+        event.preventDefault();
+        console.log(enteredValues);
+    }
+
+    function handleInputChange(event, identifier) {
+        setEnteredValues(prevValues => ({
+            ...prevValues,
+            [identifier]: event.target.value
+        }))
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <div className="control-row">
+                <div className="control no-margin">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" name="email" onChange={(event) => handleInputChange(event, 'email')} value={enteredValues.email} />
+                    <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
+                </div>
+                <div className="control no-margin">
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" name="password" onChange={(event) => handleInputChange(event, 'password')} value={enteredValues.password} />
+                </div>
+            </div>
+            <p className="form-actions">
+                <button className="button button-flat">Reset</button>
+                <button className="button" >Login</button>
+            </p>
+        </form>
+    );
+}
