581fc173ca23ee3aa589a0fa4788a07c02e5edc2
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-26 18:24:26 +0100

    lec 260: validating input upon form submission

--- 17-working-with-forms-and-user-input/01-lecture/src/components/Login.jsx
+++ 17-working-with-forms-and-user-input/01-lecture/src/components/Login.jsx
@@ -1,15 +1,25 @@
 import { useRef, useState } from "react";
 
 export default function Login() {
-   const email = useRef();
-   const password = useRef();
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit() {
     event.preventDefault();
 
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
-    console.log(enteredEmail, enteredPassword);
+
+    const emaiIsValid = enteredEmail.includes('@');
+
+    if (!emaiIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
+    console.log("Sending HTTP request");
   }
 
   return (
@@ -19,12 +29,13 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email"  ref={email} />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" ref={password}/>
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
