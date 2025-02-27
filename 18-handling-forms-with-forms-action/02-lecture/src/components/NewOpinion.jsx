f9e2222cd302ec77c488411c11637f1e43cca839
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-27 16:37:48 +0100

    lec 276: updating the ui

--- 18-handling-forms-with-forms-action/02-lecture/src/components/NewOpinion.jsx
+++ 18-handling-forms-with-forms-action/02-lecture/src/components/NewOpinion.jsx
@@ -1,6 +1,7 @@
 import { use } from 'react';
 import { useActionState } from 'react';
-import { OpinionsContext } from '../store/opinions-context'
+import { OpinionsContext } from '../store/opinions-context';
+import Submit from './Submit';
 
 export function NewOpinion() {
   const { addOpinion } = use(OpinionsContext);
@@ -66,9 +67,7 @@ export function NewOpinion() {
             {formState.errors.map(error => <li key={error}>{error}</li>)}
           </ul>)}
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        <Submit/>
       </form>
     </div>
   );
