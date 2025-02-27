afc35630974d99f1f3d5bd95d42e1758d0434e3a
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-27 13:34:24 +0100

    lec 270: form dependent state

--- 18-handling-forms-with-forms-action/01-lecture/src/components/Signup.jsx
+++ 18-handling-forms-with-forms-action/01-lecture/src/components/Signup.jsx
@@ -1,7 +1,8 @@
 import { isEmail, isNotEmpty, hasMinLength, isEqualToOtherValue } from '../util/validation'
+import { useActionState } from 'react';
 
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(prevFormState, formData) {
     const email = formData.get('email');
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm-password');
@@ -13,7 +14,7 @@ export default function Signup() {
 
     let errors = [];
 
-    if (isEmail(email)) {
+    if (!isEmail(email)) {
       errors.push('Invalid email address.');
     }
 
@@ -40,10 +41,18 @@ export default function Signup() {
     if (acquisitionChannel.length === 0) {
       errors.push('Please select at least one acquisition channel.')
     }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    return { errors: null }
   }
 
+  const [formState, formAction, pending] = useActionState(signupAction, { errors: null });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -127,6 +136,10 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && <ul className='error'>
+        {formState.errors.map(error => <li key={error}>{error}</li>)}
+      </ul>}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
