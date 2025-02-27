acf30fd46561ed976090f975896225e717eea80e
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-02-27 17:03:02 +0100

    lec 280: optimistic updating

--- 18-handling-forms-with-forms-action/02-lecture/src/components/Opinion.jsx
+++ 18-handling-forms-with-forms-action/02-lecture/src/components/Opinion.jsx
@@ -1,15 +1,19 @@
 import { use } from "react";
 import { OpinionsContext } from "../store/opinions-context";
-import { useActionState } from "react";
+import { useActionState, useOptimistic } from "react";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
 
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(votes, (prevVotes, mode) => mode === 'up' ? prevVotes + 1 : prevVotes - 1);
+
   async function upvoteAction() {
+    setVotesOptimistically('up');
     await upvoteOpinion(id);
   }
 
   async function downvoteAction() {
+    setVotesOptimistically('down');
     await downvoteOpinion(id);
   }
 
@@ -41,7 +45,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button formAction={downvoteFormAction} disabled={upvotePending || downvotePending}>
           <svg
