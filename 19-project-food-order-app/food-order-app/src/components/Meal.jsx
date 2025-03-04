6b291b8b025288e86528a01a25142b02acd4736e
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-04 13:33:37 +0100

    format price

--- 19-project-food-order-app/food-order-app/src/components/Meal.jsx
+++ 19-project-food-order-app/food-order-app/src/components/Meal.jsx
@@ -1,10 +1,12 @@
+import { currencyFormatter } from "../util/formatting";
+
 export default function Meal({ meal: { id, description, image, name, price } }) {
     return (
         <article className="meal-item">
             <img src={`http://localhost:3000/${image}`} alt={name} ></img>
             <div>
                 <h3>{name}</h3>
-                <p className="meal-item-price">{price}</p>
+                <p className="meal-item-price">{currencyFormatter.format(price)}</p>
                 <p className="meal-item-description">{description}</p>
             </div>
             <p className="meal-item-actions">
