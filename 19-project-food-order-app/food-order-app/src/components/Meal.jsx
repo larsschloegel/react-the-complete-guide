6db9f5ddf36c057d7fa0575643544114dc6e2436
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-04 13:30:50 +0100

    add details of meal

--- 19-project-food-order-app/food-order-app/src/components/Meal.jsx
+++ 19-project-food-order-app/food-order-app/src/components/Meal.jsx
@@ -1,8 +1,15 @@
-export default function Meal( { meal : {id, description, image, name, price}}) {
-    return(
+export default function Meal({ meal: { id, description, image, name, price } }) {
+    return (
         <article className="meal-item">
-            <img src={`http://localhost:3000/${image}`} alt={id} ></img>
-            <h3>{name}</h3>
+            <img src={`http://localhost:3000/${image}`} alt={name} ></img>
+            <div>
+                <h3>{name}</h3>
+                <p className="meal-item-price">{price}</p>
+                <p className="meal-item-description">{description}</p>
+            </div>
+            <p className="meal-item-actions">
+                <button className="button">Add to cart</button>
+            </p>
         </article>
     )
 }
