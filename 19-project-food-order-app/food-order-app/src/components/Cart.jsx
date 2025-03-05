5907ce5ac6ea2e4c933b060a6d843679b5719f74
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-05 14:47:53 +0100

    lec 293: go to checkou only when items in cart

--- 19-project-food-order-app/food-order-app/src/components/Cart.jsx
+++ 19-project-food-order-app/food-order-app/src/components/Cart.jsx
@@ -19,13 +19,15 @@ export default function Cart() {
         <h2>Your Cart</h2>
         <ul>
             {cart.items.map((item) => (
-                <CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.quantity} onDecrease={() => cart.removeItem(item.id)} onIncrease={() => cart.addItem(item)}/>
+                <CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.quantity} onDecrease={() => cart.removeItem(item.id)} onIncrease={() => cart.addItem(item)} />
             ))}
         </ul>
         <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
         <p className="modal-actions">
             <Button textOnly onClick={handleCloseCart}>Close</Button>
-            <Button onClick={handleCloseCart}>Go to Checkout</Button>
+            {cart.items.length > 0 && (
+                <Button onClick={handleCloseCart}>Go to Checkout</Button>
+            )}
         </p>
     </Modal>
 }
