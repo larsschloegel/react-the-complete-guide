6b585f53c08e0d8ba02130c6a17d76bf03f6bb1c
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-05 17:17:38 +0100

    lec 297: handling error states

--- 19-project-food-order-app/food-order-app/src/components/Meals.jsx
+++ 19-project-food-order-app/food-order-app/src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import Meal from "./Meal";
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 
 const requestConfig = {};
 
@@ -8,7 +9,11 @@ export function Meals() {
     const {data: meals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, []);
 
     if (isLoading){
-        return <p>Fetching meals...</p>;
+        return <p className="center">Fetching meals...</p>;
+    }
+
+    if (error) {
+        return <Error title='Failed to fetch meals' message={error}/>;
     }
 
     return (
