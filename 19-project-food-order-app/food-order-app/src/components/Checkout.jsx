0e12cfa37de45b61ff301ccf4d07ecc784cb0917
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-05 17:44:16 +0100

    lec 299: migrate to form actions

--- 19-project-food-order-app/food-order-app/src/components/Checkout.jsx
+++ 19-project-food-order-app/food-order-app/src/components/Checkout.jsx
@@ -33,13 +33,10 @@ export default function Checkout() {
         clearData();
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        const fd = new FormData(event.target);
+    async function checkoutAction(fd) {
         const customerData = Object.fromEntries(fd.entries());
 
-        sendRequest(JSON.stringify({
+        await sendRequest(JSON.stringify({
             order: {
                 items: cart.items,
                 customer: customerData
@@ -71,7 +68,7 @@ export default function Checkout() {
 
     return (
         <Modal open={userProgress.progress === 'checkout'} onClose={handleClose}>
-            <form onSubmit={handleSubmit}>
+            <form action={checkoutAction}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
                 <Input label="Full name" type="text" id="name" />
