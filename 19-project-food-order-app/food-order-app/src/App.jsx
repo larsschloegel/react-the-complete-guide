9a51297ba0a3608a13521c8f068a38ce0f88cc28
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-05 17:00:14 +0100

    lec 296: hook for http request

--- 19-project-food-order-app/food-order-app/src/App.jsx
+++ 19-project-food-order-app/food-order-app/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import { Meals } from "./components/Meals";
 import { CartContextProvider } from "./store/cart-context";
-import { MealContextProvider } from "./store/meal-context";
 import { UserProgressContextProvider } from "./store/user-progress-context";
 import Cart from "./components/Cart";
 import Checkout from "./components/Checkout";
@@ -13,11 +12,9 @@ function App() {
       <CartContextProvider>
         <Header />
         <main>
-          <MealContextProvider>
             <Meals/>
             <Cart/>
             <Checkout/>
-          </MealContextProvider>
         </main>
       </CartContextProvider>
     </UserProgressContextProvider>
--- 19-project-food-order-app/food-order-app/src/components/Meals.jsx
+++ 19-project-food-order-app/food-order-app/src/components/Meals.jsx
@@ -1,13 +1,19 @@
-import { MealContext } from "../store/meal-context";
-import { use } from 'react';
 import Meal from "./Meal";
+import useHttp from "../hooks/useHttp";
+
+const requestConfig = {};
 
 export function Meals() {
-    const { meals } = use(MealContext);
+
+    const {data: meals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+    if (isLoading){
+        return <p>Fetching meals...</p>;
+    }
 
     return (
         <ul id="meals">
-            {meals && meals.map((meal) => (
+            {meals.map((meal) => (
                     <li key={meal.id} className="meal-item">
                         <Meal meal={meal} />
                     </li>
