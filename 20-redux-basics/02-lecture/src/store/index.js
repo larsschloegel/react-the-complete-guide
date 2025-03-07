e6f28549c9d5885a0fb623dfc818f656a1c388be
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-07 10:08:09 +0100

    lec 315: working with multiple state properties

--- 20-redux-basics/02-lecture/src/store/index.js
+++ 20-redux-basics/02-lecture/src/store/index.js
@@ -1,13 +1,32 @@
 import {createStore} from 'redux';
 
-const counterReducer = (state = {counter: 0}, action) => {
+const initialState = {
+    counter: 0,
+    showCounter: true
+}
+
+const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'increment':
-            return {counter: state.counter + 1,}
+            return {
+                counter: state.counter + 1,
+                showCounter: state.showCounter
+            }
         case 'decrement':
-            return {counter: state.counter - 1,}
+            return {
+                counter: state.counter - 1,
+                showCounter: state.showCounter
+            }
         case 'increase':
-            return {counter: state.counter + action.payload,}
+            return {
+                counter: state.counter + action.payload,
+                showCounter: state.showCounter
+            }
+        case 'toggle':
+            return {
+                counter: state.counter,
+                showCounter: !state.showCounter
+            }
         default:
             return state;
     }
