204898aa5a459c2908f03a6cbf6b407bab0d9736
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-17 13:21:28 +0100

    lec 587: generics

--- 32-react-typescript/00-template/basics.ts
+++ 32-react-typescript/00-template/basics.ts
@@ -64,3 +64,14 @@ function add(a: number, b: number): number {
 function print(value: any) {
     console.log(value);
 }
+
+// Generics
+
+function insertAtBeginnig<T>(array: T[], value: T) {
+    return [value, ...array];
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginnig(demoArray, -1); // [-1,1,2,3]
+const stringArray = insertAtBeginnig(['a', 'b', 'c'], 'd');
