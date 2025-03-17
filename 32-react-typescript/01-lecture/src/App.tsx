493f0d0ded0275411d9f0c20d60a6d988e60dbe0
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-17 16:05:32 +0100

    lec 597: managing state & typeScript

--- 32-react-typescript/01-lecture/src/App.tsx
+++ 32-react-typescript/01-lecture/src/App.tsx
@@ -1,16 +1,16 @@
 import Todos from "./components/Todos.tsx";
 import Todo from "./models/todo.ts";
 import NewTodo from "./components/NewTodo.tsx";
+import {useState} from "react";
 
 function App() {
-
-    const todos = [
-        new Todo('Learn React'),
-        new Todo('Learn TypeScript')
-    ];
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     const addTodoHandler = (todoText: string) => {
-
+        const newTodo = new Todo(todoText);
+        setTodos((prevTodos) => {
+            return prevTodos.concat(newTodo);
+        });
     };
 
     return (
