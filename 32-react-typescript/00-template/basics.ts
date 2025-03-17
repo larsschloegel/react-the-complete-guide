39321e4a331b625ef48cbab5399a12fc912d106a
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-17 09:55:06 +0100

    lec 582: working with array & object types

---
+++ 32-react-typescript/00-template/basics.ts
@@ -0,0 +1,37 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+//Primitives
+
+let age: number = 23;
+
+age = 20;
+
+let userName: string;
+
+userName = "John";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Music', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: "John",
+    age: 30
+};
+
+/*person = {
+    isEmployee: true,
+}*/
