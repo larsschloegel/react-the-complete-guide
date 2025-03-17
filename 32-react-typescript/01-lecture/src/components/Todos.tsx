ea61439e2fe6860b1a8d785f9702c90119023bfb
STEPSTONE-ASA\lars.schloegel <lars.schloegel@stepstone.com>
2025-03-17 15:01:41 +0100

    lec 593: component for todo

--- 32-react-typescript/01-lecture/src/components/Todos.tsx
+++ 32-react-typescript/01-lecture/src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../models/todo.ts";
+import TodoItem from "./TodoItem.tsx";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <ul>
-            {props.items.map((item) => (<li key={item.id}>{item.text}</li>))}
+            {props.items.map((item) => (<TodoItem key={item.id} text={item.text}/>))}
         </ul>
     );
 }
